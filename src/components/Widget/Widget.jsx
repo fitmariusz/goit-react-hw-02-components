ea250f1327080758191a9c92412efc8a910ea306
Mariusz Fit <72906237+fitmariusz@users.noreply.github.com>
2024-04-13 05:45:25 +0200

    Task1 step finish

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,34 +1,43 @@
-import { App } from "components/App";
-import React from "react";
+import { useState } from "react";
+import css from "./Widget.module.css"
 
+ 
 
-export const Widget = ({ step=1 }) => { 
+export const Widget = () => { 
+    const [valueGood, setGoodValue] = useState(0);
+    const [valueNeutral, setNeutralValue] = useState(0);
+    const [valueBad, setBadValue] = useState(0);
 
-    const onClickGood = ({ }) => {
-        console.log("good");
+    const onClickGood = () => {
+        setGoodValue(prevValue => prevValue += 1)
+        console.log("good" );
     }
 
-    const onClickNeutral = ({ }) => {
+    const onClickNeutral = () => {
+        setNeutralValue(prevValue => prevValue += 1)
         console.log("neutral");
     }
 
-    const onClickBad = ({ }) => {
+    const onClickBad = () => {
+        setBadValue(prevValue => prevValue += 1)
         console.log("bad");
     }
 
 
 return (
     <>
-        <div>    
-        
-            <button type="button" onClick={onClickGood}>Good {step}</button>
-            <button type="button" onClick={(onClickNeutral)}>Neutral {step}</button>
-            <button type="button" onClick={onClickBad}>Bad {step}</button>
-        
-            <div>
-                <span>Good: </span><span>0</span>
-                <span>Neutral: </span><span>0</span>
-                <span>Bad: </span><span>0</span>
+        <div>
+            <h2>Please leave feedback</h2>
+            <div className={css.feedback}> 
+                <button type="button" className={css.buttonFeedback} onClick={onClickGood}>Good</button>
+                <button type="button" className={css.buttonFeedback} onClick={(onClickNeutral)}>Neutral</button>
+                <button type="button" className={css.buttonFeedback} onClick={onClickBad}>Bad</button>
+            </div>
+            <h2>Statistics</h2>
+            <div className={css.statistic}>
+                <li className={css.liStatistic}>Good: <span>{valueGood}</span></li>
+                <li className={css.liStatistic}>Neutral: <span>{valueNeutral}</span></li>
+                <li className={css.liStatistic}>Bad: <span>{valueBad}</span></li>
             </div>
         </div>
     </>
