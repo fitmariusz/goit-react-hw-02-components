bf12adc74e89e0962be13f4b99de283fd59498f3
Mariusz Fit <72906237+fitmariusz@users.noreply.github.com>
2024-04-13 05:59:04 +0200

    add App.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
+import './App.css'
 import { Widget } from "./Widget/Widget";
+
+
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#fff',
-        backgroundColor:'#000',
-      }}
-    >
+    <div>
       <Widget></Widget>
     </div>
   );
