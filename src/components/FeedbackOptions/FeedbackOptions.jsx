bc58293260b55cc1def7aec10f7629de2baf7ea3
Mariusz Fit <72906237+fitmariusz@users.noreply.github.com>
2024-04-16 03:21:56 +0200

    Statistic and onClick funktion

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+
+// import css from "./Widget.module.css"
+
+ 
+
+// export const FeedbackOptions = (props) => {
+//     const { options } = props;
+ 
+// return (
+//     <>
+//         <div className={css.feedback}> 
+//             {options.map(option => (<button type="button" key="good" className={css.buttonFeedback} onClick={onClickGood}>Good</button>)
+                
+//             )}
+//     </>
+//     );
+
+// };
